2343174f4a67fea2c926488b1d7bb435b2bf58db
erik fredericks <frederer@gvsu.edu>
2026-06-02 17:09:02 -0400

    gptp

--- assets/marp-slides/convert-all.bash
+++ assets/marp-slides/convert-all.bash
@@ -8,12 +8,12 @@ if [[ $# -ne 1 ]]; then
     fname="${f%%.*}"
     echo "Converting $fname"
     npx @marp-team/marp-cli@latest --theme rose-pine.css $f --allow-local-files --pdf -o $odir/$fname.pdf
-    npx @marp-team/marp-cli@latest --theme rose-pine.css $f --allow-local-files -o $odir/$fname.html
+    npx @marp-team/marp-cli@latest --theme rose-pine.css --html $f --allow-local-files -o $odir/$fname.html
   done
 else
   f=$1
   fname="${f%%.*}"
   echo "Converting $fname"
   npx @marp-team/marp-cli@latest --theme rose-pine.css $f --allow-local-files --pdf -o $odir/$fname.pdf
-  npx @marp-team/marp-cli@latest --theme rose-pine.css $f --allow-local-files -o $odir/$fname.html
+  npx @marp-team/marp-cli@latest --theme rose-pine.css $f --html --allow-local-files -o $odir/$fname.html
 fi
